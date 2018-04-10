005e99c48fbdac7a215d6017d3ccfad40d048be5
Min Kim <min.c.kim@gmail.com>
2018-04-10 13:51:22 -0500

    Added .js files for Leaflet Plugins

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // query the website for geojson
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+d3.json(queryUrl, function (data) {
   // function to create map on features of the geojson
   createFeatures(data.features);
 });
@@ -17,34 +17,32 @@ function createFeatures(earthquakeData) {
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
-  
+
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature,
-    pointToLayer: function (feature, latlng){
+    pointToLayer: function (feature, latlng) {
       //create circle marker instead of regular marker
-      return L.circleMarker(latlng,{
+      return L.circleMarker(latlng, {
         //run the radius function to generate a proportional radius for the marker based on eq mag
         radius: getRadius(feature.properties.mag),
         //set color scheme for the magnitutdes
         color: getColor(feature.properties.mag)
-      }
-    )}
+      })
+    }
   });
 
   // run createMap function
   createMap(earthquakes);
 }
-
+var tectonicPlates = new L.LayerGroup();
 function createMap(earthquakes) {
 
   // Define layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
-    "T6YbdDixkOBWH_k9GbS8JQ");
+    "access_token=pk.eyJ1IjoibWluY2tpbTEyMjIiLCJhIjoiY2pkaGp5NHR0MHd3eDMxbnF6bXlsazhxYiJ9.6WOQPTje5_AYqQO_4W36xQ");
 
   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
-    "T6YbdDixkOBWH_k9GbS8JQ");
+    "access_token=pk.eyJ1IjoibWluY2tpbTEyMjIiLCJhIjoiY2pkaGp5NHR0MHd3eDMxbnF6bXlsazhxYiJ9.6WOQPTje5_AYqQO_4W36xQ");
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
@@ -54,7 +52,8 @@ function createMap(earthquakes) {
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquakes
+    Earthquakes: earthquakes,
+    "Tectonic Plates": tectonicPlates
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -74,36 +73,51 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
   //define legend variable
-var legend = L.control({position: 'bottomright'});
+  var legend = L.control({
+    position: 'bottomright'
+  });
 
-legend.onAdd = function (map) {
+  legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 2, 4, 6, 8],
-        labels = [];
+      grades = [0, 2, 4, 6, 8],
+      labels = [];
 
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+      div.innerHTML +=
+        '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
 
     return div;
-};
+  };
 
-legend.addTo(myMap);
+  legend.addTo(myMap);
+  //function to create tectonic plates
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
+    function(platedata) {
+      // Adding our geoJSON data, along with style information, to the tectonicplates
+      // layer.
+      L.geoJson(platedata, {
+        color: "orange",
+        weight: 2
+      })
+      .addTo(tectonicPlates);
+
+      // Then add the tectonicplates layer to the map.
+      tectonicPlates.addTo(myMap);
+    });
 }
 //create a function to magnify the radius
-function getRadius(data){
+function getRadius(data) {
   return data * 5
 }
 //create a function to separate the data into color groups based on mag
 function getColor(data) {
   return data > 8 ? '#b30000' :
-         data > 6  ? '#e34a33' :
-         data > 4  ? '#045a8d' :
-         data > 2 ? '#2b8cbe' :
-                    '#74a9cf';
+    data > 6 ? '#e34a33' :
+    data > 4 ? '#045a8d' :
+    data > 2 ? '#2b8cbe' :
+    '#74a9cf';
 }
-
